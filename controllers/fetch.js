a06ea5405c19813272ea1a7a031d7d6c23031ee6
Nathan <nathanalston@gmail.com>
2018-04-06 00:04:34 -0400

    Fetch Routes

--- controllers/fetch.js
+++ controllers/fetch.js
@@ -49,8 +49,94 @@ router.get("/scrape", function(req, res) {
 ////////Route to get articles from DB///////////////////
 ////////////////////////////////////////////////////////
 
+router.get("/articles", function(req, res) {
 
-// Grab an article by it's ObjectId
+  // Grab every doc in the Articles array
+  Article.find({})
+
+  // Execute the above query
+  .exec(function(err, doc) {
+
+    // Log any errors
+    if (err) {
+      console.log(error);
+    }
+
+    // Or send the doc to the browser as a json object
+    else {
+      res.json(doc);
+    }
+  });
+});
+
+
+////////////////////////////////////////////////////////
+//////////////////////Save an article///////////////////
+////////////////////////////////////////////////////////
+
+
+router.post("/save/:id", function(req, res) {
+
+  // Use the article id to find and update it's saved property to true
+  Article.findOneAndUpdate({ "_id": req.params.id }, { "saved": true })
+
+  // Execute the above query
+  .exec(function(err, doc) {
+
+    // Log any errors
+    if (err) {
+      console.log(err);
+    }
+
+    // Log result
+    else {
+      console.log("doc: ", doc);
+    }
+  });
+});
+
+
+
+////////////////////////////////////////////////////////
+///////////////////Create a Comment/////////////////////
+////////////////////////////////////////////////////////
+
+
+router.post("/comment/:id", function(req, res) {
+  // Create a new comment and pass the req.body to the entry
+  var newComment = new Comment(req.body);
+  // And save the new comment the db
+  newComment.save(function(error, newComment) {
+    // Log any errors
+    if (error) {
+      console.log(error);
+    }
+    // Otherwise
+    else {
+      // Use the article id to find and update it's comment
+      Article.findOneAndUpdate({ "_id": req.params.id }, { $push: { "comments": newComment._id }}, { new: true })
+      // Execute the above query
+      .exec(function(err, doc) {
+        // Log any errors
+        if (err) {
+          console.log(err);
+        }
+        else {
+          console.log("doc: ", doc);
+          // Or send the document to the browser
+          res.send(doc);
+        }
+      });
+    }
+  });
+});
+
+
+////////////////////////////////////////////////////////
+///////////////////Get Object by ID/////////////////////
+////////////////////////////////////////////////////////
+
+
 router.get("/articles/:id", function(req, res) {
   // Using the id passed in the id parameter, prepare a query that finds the matching one in our db...
   Article.findOne({ "_id": req.params.id })
@@ -68,3 +154,29 @@ router.get("/articles/:id", function(req, res) {
     }
   });
 });
+
+
+////////////////////////////////////////////////////////
+/////////////Remove a saved article/////////////////////
+////////////////////////////////////////////////////////
+
+
+router.post("/unsave/:id", function(req, res) {
+  // Use the article id to find and update it's saved property to false
+  Article.findOneAndUpdate({ "_id": req.params.id }, { "saved": false })
+  // Execute the above query
+  .exec(function(err, doc) {
+    // Log any errors
+    if (err) {
+      console.log(err);
+    }
+    // Log result
+    else {
+      console.log("Article Removed");
+    }
+  });
+  res.redirect("/saved");
+});
+
+
+module.exports = router;
