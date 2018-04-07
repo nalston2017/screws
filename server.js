db2efef1a0be089bacf80c7e6e66319941af6c7c
Nathan <nathanalston@gmail.com>
2018-04-07 12:54:14 -0400

    Update server.js

--- server.js
+++ server.js
@@ -7,6 +7,10 @@ var mongoose = require('mongoose');
 var request = require('request');
 var cheerio = require('cheerio');
 var exphbs = require("express-handlebars");
+var Note = require("./models/Node.js");
+var Headline = require("./models/Headline.js");
+var htmlRouter = require("./controllers/headline.js");
+var articleRouter = require("./controllers/fetch.js");
 
 // Initialize Express
 var port = process.env.PORT || 3000;
@@ -39,77 +43,6 @@ db.on('error', function(err) {
 db.once('open', function() {
   console.log('Mongoose connection successful.');
 });
-
-// Database configuration
-var databaseUrl = "scraper";
-var collections = ["scrapedData"];
-
-// Hook mongojs configuration to the db variable
-var db = mongojs(databaseUrl, collections);
-db.on("error", function(error) {
-  console.log("Database Error:", error);
-});
-
-// Main route (simple Hello World Message)
-app.get("/", function(req, res) {
-  res.send("Hello world");
-});
-
-// Retrieve data from the db
-app.get("/all", function(req, res) {
-  // Find all results from the scrapedData collection in the db
-  db.scrapedData.find({}, function(error, found) {
-    // Throw any errors to the console
-    if (error) {
-      console.log(error);
-    }
-    // If there are no errors, send the data to the browser as json
-    else {
-      res.json(found);
-    }
-  });
-});
-
-// Scrape data from one site and place it into the mongodb db
-app.get("/scrape", function(req, res) {
-  // Make a request for the news section of ycombinator
-  request("http://www.espn.com/", function(error, response, html) {
-    // Load the html body from request into cheerio
-    var $ = cheerio.load(html);
-    // For each element with a "title" class
-    $("article").each(function(i, element) {
-      // Save the text and href of each link enclosed in the current element
-      var headline = $(element).children("a").text();
-      var summary;
-      var url = $(element).children("a").attr("href");
-
-
-      // If this found element had both a title and a link
-      if (headline && summary && url) {
-        // Insert the data in the scrapedData db
-        db.scrapedData.insert({
-          headline: headline,
-          summary: summary.
-          url: url
-        },
-        function(err, inserted) {
-          if (err) {
-            // Log the error if one is encountered during the query
-            console.log(err);
-          }
-          else {
-            // Otherwise, log the inserted data
-            console.log(inserted);
-          }
-        });
-      }
-    });
-  });
-
-  // Send a "Scrape Complete" message to the browser
-  res.send("Scrape Complete");
-});
-
 
 // Listen on port 3000
 app.listen(3000, function() {
